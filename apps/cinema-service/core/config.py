c0464cd2fa44e6be5bf69281ad769266d6a271f6
Damian <damiankowalczyk3688@gmail.com>
2026-08-06 11:47:53 +0200

    feat: add postgresql settings for cinema-service

--- apps/cinema-service/core/config.py
+++ apps/cinema-service/core/config.py
@@ -1,13 +1,23 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
-    port: int = 8001
+class DatabaseSettings(BaseSettings):
+
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str
+    postgres_port: int
+    postgres_db: str
+
+    @property
+    def POSTGRES_URI(self) -> str:
+        return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+
 
     model_config = SettingsConfigDict(
-        env_file='.env',
+        env_file=".env",
         extra="ignore",
         frozen=True
     )
 
-settings = Settings()
+database_settings = DatabaseSettings()
