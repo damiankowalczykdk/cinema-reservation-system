5db4720dd214eb9007e8171d3f6dae4d23e2ecf2
Damian <damiankowalczyk3688@gmail.com>
2026-08-17 20:27:54 +0200

    refactor: move routes under api/routes package

--- apps/cinema-service/main.py
+++ apps/cinema-service/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-from routes.health import router as health_router
+from api.routes.cinema import router as cinema_router
+from api.routes.health import router as health_router
+
 
 app = FastAPI()
 
+app.include_router(cinema_router)
 app.include_router(health_router)
